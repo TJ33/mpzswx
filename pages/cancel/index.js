1ec656de8e172afc80127d8a7b4cc5ef9fa715e0
username <870409697@qq.com>
2020-02-17 14:32:42 +0800

    完善

--- pages/cancel/index.js
+++ pages/cancel/index.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    reason: [{ name: '司机态度恶劣，服务差', value: '' }, { name: '计划有变，已不需要服务', value: '' }, { name: '司机爽约、乱抢单', value: '' }, { name: '司机告诉我要等很久(大于15分钟)', value: '' }, { name: '所选车型装不下我的货物', value: '' }, { name: '司机让我私下联系', value: '' }, { name: '司机乱开价(装卸费等)', value: '' }, { name: '联系不上司机', value: '' }, { name: '我已经找到其他车了', value: '' }],
   },
 
   /**
