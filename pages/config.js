b3fe2f3ed9671c093d76359ebca2ec68cf4ea161
username <870409697@qq.com>
2020-03-07 11:27:49 +0800

    完善

--- pages/config.js
+++ pages/config.js
@@ -1,9 +1,9 @@
 // export let domain = "http://192.168.7.119:9014"
-// export let domain = "http://zs.51qp.top"
-export let domain = "http://localhost:9014"
+export let domain = "http://zs.51qp.top"
+// export let domain = "http://localhost:9014"
 // export let domain = "https://tms.51qp.top"
 
 
 
-export let APPID = "wxccd5f8fd7806fd16"
+export let APPID = "wxfea63eb860bd639e"
 export let SECRET = "939e0dacbc5d3eb28cb89027637a09be"
